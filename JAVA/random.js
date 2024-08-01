bfb8b8d1db363937eaf797b1f5437049b121835b
pedrofolador <106595220+pedrofolador@users.noreply.github.com>
2024-08-01 19:59:29 -0300

    small mistake

--- JAVA/random.js
+++ JAVA/random.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["você"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
